8478d0ffc7ce2a9051489d37a32abc5cd7d36130
Ritesh Patel <patel2000ritesh@gmail.com>
2020-07-14 13:29:55 -0400

    Added eventlisteners

--- app.js
+++ app.js
@@ -1,23 +1,52 @@
+//BUDGET MOODULE
 var budgetController = (function(){
 
-    //Code
+   //Code
 
 })()
 
 
 
-
+//UI MODULE
 var UIController = (function(){
 
     //Code
 
 })();
 
-
+//GLOBAL MODULE
 var controller = (function(budget, UI){
 
 
-    //Code
+    var controlAddItem = function(){
+        // 1. Get the field input data
+
+        // 2. Add the item to the budget controller
+
+        // 3. Add the item to the UI
+
+        // 4. Calculate the budget 
+
+        // 5. Display the budget on UI
+
+        console.log("working")
+    }
+
+
+    document.querySelector('.add__btn').addEventListener('click', controlAddItem);
+
+
+    document.addEventListener('keypress', function(event){
+
+        if (event.keyCode === 13 || event.which === 13) {
+            controlAddItem();
+        }
+
+    })
+
+
+
+
 
 })(budgetController, UIController);
 
